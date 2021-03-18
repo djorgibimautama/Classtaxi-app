a67c7fef7cb6a9206394922e1541c6931e5b22e6
djorgibimautama <djorgibima@gmail.com>
2021-03-18 21:19:00 +0700

    Classtaxi-app

---
+++ classtaxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("Onduty : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger {0}:", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
